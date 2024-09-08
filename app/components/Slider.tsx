54240cbace26bb00c2e701d6394d9da0d8318fe7
mehrab786 <mehrabtarrar@gmail.com>
2024-09-08 07:01:38 +0400

    6TH COMMIT

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -1,13 +1,74 @@
-import React from 'react'
+"use client"
+
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react'
+
+const slides = [
+  {
+    id: 1,
+    title: "Summer Sale Collections",
+    description: "Sale! Up to 50% off!",
+    img: "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800",
+    url: "/",
+    bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+  },
+  {
+    id: 2,
+    title: "Winter Sale Collections",
+    description: "Sale! Up to 50% off!",
+    img: "https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800",
+    url: "/",
+    bg: "bg-gradient-to-r from-pink-50 to-blue-50",
+  },
+  {
+    id: 3,
+    title: "Spring Sale Collections",
+    description: "Sale! Up to 50% off!",
+    img: "https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800",
+    url: "/",
+    bg: "bg-gradient-to-r from-blue-50 to-yellow-50",
+  },
+];
+
+
 
 const Slider = () => {
+  const [current, setCurrent] = useState(0);
   return (
-    <div>Slider
-
-    
-
-    </div>
-  )
-}
+      <div className="h-[calc(100vh-90px)] overflow-hidden bg-slate-800">
+        <div className="w-max h-full transition-all ease-in-out duration-1000 ">
+          {slides.map(slide =>(
+            <div className= {`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`} key={slide.id}>
+              {/* text-container */}
+              <div className="h-1/2 xl:w-1/2 flex flex-col gap-8 justify-center items-center text-center 2xl:gap-12 xl:h-full">
+                <h2 className="text-xl lg:text-3xl xl:text-5xl"> {slide.description}</h2>
+                <h1 className="text-5xl lg:text-6xl xl:text-8xl font-semibold">{slide.title}</h1>
+                <Link href={slide.url}> 
+                  <button className='rounded-md bg-black text-white py-3 px-4'>Shop Now</button>
+                </Link>
+              </div>
+              {/* image-conatainer */}
+              <div className="relative h-1/2 xl:w-1/2 xl:h-full"> 
+                <Image src={slide.img} alt='image' fill sizes='100%' className='object-cover' />
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
+         {
+          slides.map((slide,index) =>(
+            <div className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${current===index ? "scale-150" : ""}`}
+            key={slide.id}
+            onClick={()=> setCurrent(index)}
+            >
+              { current === index && (<div className="w-[6px] h-[6px] rounded-full bg-gray-600"> </div>)}
+            </div>
+          ))
+          }
+          </div>
+      </div>
+  );
+};
 
 export default Slider
