b745efdf972ccfd961d445129f21403e613bba3d
mehrab786 <mehrabtarrar@gmail.com>
2024-10-28 03:08:23 +0400

    CI/CD TEST2

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -10,7 +10,7 @@ const listPage = () => {
       <div className="hidden bg-pink-50  sm:flex  flex-row justify-between h-64 ">
         <div className="w-2/3 flex flex-col px-4 py-6 items-center justify-between gap-8">
             <h1 className="text-4xl font-semibold leading-[48px] text-gray-700 ">
-              Grab up to 500% off on <br /> selected products 
+              Grab up to 50% off on <br /> selected products 
             </h1>
             <button className="rounded-3xl bg-red-500 text-white px-5 py-3 w-max text-sm">Buy Now</button>
         </div>
